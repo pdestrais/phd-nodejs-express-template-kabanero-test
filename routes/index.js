67f2973df4c3a745f7a9038789878073855341b3
Philippe Destrais <philippe_destrais@be.ibm.com>
2019-10-30 11:23:13 +0100

    test to start building with Kabanero

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     console.log('test console.log');
     res.render('index', {
-        msg: "Hello my new Express app!!!"
+        msg: "Hello my new Express app build using Kabanero!!!"
     });
 });
 
